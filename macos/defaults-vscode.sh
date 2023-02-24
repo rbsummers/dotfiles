9ddc6979e1c3113abc9ff7ee8d2ad1ceed42a326
rbsummers <rbsummers@users.noreply.github.com>
2023-02-24 15:19:53 +0100

    feat(vscode): keybindings

--- macos/defaults-vscode.sh
+++ macos/defaults-vscode.sh
@@ -1,3 +1,4 @@
 VSCODE_CONFIG_FOLDER="${HOME}/Library/Application Support/Code/User"
 
 ln -s "${XDG_CONFIG_HOME}/vscode/settings.json" "${VSCODE_CONFIG_FOLDER}/settings.json"
+ln -s "${XDG_CONFIG_HOME}/vscode/keybindings.json" "${VSCODE_CONFIG_FOLDER}/keybindings.json"
